292b3e7385cf180ebf87e0eaded98061ad6fdd70
Renzo Ariel Posobon <renzoposobon@gmail.com>
2022-08-30 22:57:35 -0300

    desafio array

--- js/index.js
+++ js/index.js
@@ -1,30 +1,21 @@
-/*CARRITO DE COMPRAS {}*/
+/*CARRITO DE COMPRAS*/
 
-let ingreso = prompt(`Ingresar producto al carrito \n1- Combo perro \n2- Combo gato \n3-Combo mixto`);
+let ingreso
 let costo;
 let producto;
 let codigoDescuento = 589125;
 
-function descuento(){
-    let descuento = prompt("Posee algun descuento?")
-    if (descuento == 'si' || (descuento) == "SI" || (descuento) == "Si"){
-        let ingresoDescuento = parseInt(prompt("Ingrese codigo"))
-        if (ingresoDescuento == codigoDescuento){
-            alert("Su codigo ha sido aceptado!")
-            costo = costo - 2
-            alert(`El ${producto} cuesta $${costo}`)
-        } else {
-            alert('codigo invalido')
-        }
-    } else if (descuento == 'no' || (descuento) == "NO" || (descuento) == "No"){
-        alert(`El ${producto} cuesta $${costo}`)
-    }
-    else {
-        alert("Opcion incorrecta")
+class Personas {
+    constructor(producto, costo){
+        this.producto = producto,
+        this.costo = costo
     }
 }
 
-function agregar(ingreso) {
+var arr = []
+
+for (let i = 0; i < 5; i++) {
+    let ingreso = prompt(`Ingresar producto al carrito \n1- Combo perro \n2- Combo gato \n3-Combo mixto`);
     if (ingreso == 1){
         costo = 10;
         producto = "Combo perro";
@@ -40,6 +31,50 @@ function agregar(ingreso) {
     } else {
         alert("Opcion incorrecta")
     }
+    var objeto1 = new Personas(producto, costo)
+    arr.push(objeto1)
+    alert(`Su ${objeto1.producto} cuesta ${objeto1.costo}`)
 }
 
-agregar(ingreso)
+for (let i = 0; i < arr.length; i++){
+    alert(`OBJETO ${i} \nPRODUCTO ${arr[i].producto} \nPRECIO ${arr[i].costo}`)
+}
+
+function descuento(){
+    let descuento = prompt("Posee algun descuento?")
+    if (descuento == 'si' || (descuento) == "SI" || (descuento) == "Si"){
+        let ingresoDescuento = parseInt(prompt("Ingrese codigo"))
+        if (ingresoDescuento == codigoDescuento){
+            alert("Su codigo ha sido aceptado!")
+            costo = costo - 2
+            // alert(`El ${producto} cuesta $${costo}`)
+        } else {
+            alert('codigo invalido')
+        }
+    } else if (descuento == 'no' || (descuento) == "NO" || (descuento) == "No"){
+        // alert(`El ${producto} cuesta $${costo}`)
+    }
+    else {
+        alert("Opcion incorrecta")
+    }
+}
+
+// function agregar(ingreso) {
+//     if (ingreso == 1){
+//         costo = 10;
+//         producto = "Combo perro";
+//         descuento()
+//     } else if (ingreso == 2){
+//         costo = 8;
+//         producto = "Combo gato";
+//         descuento()
+//     } else if (ingreso == 3){
+//         producto = "Combo mixto";
+//         costo = 15;
+//         descuento()
+//     } else {
+//         alert("Opcion incorrecta")
+//     }
+// }
+
+// agregar(ingreso)
