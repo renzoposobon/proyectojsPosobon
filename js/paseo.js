7b301dfc1a7f965037b9b454f26a404fe6abebac
Renzo Posobon <renzoposobon95@gmail.com>
2022-10-08 12:05:49 -0300

    carta paseadores

--- js/paseo.js
+++ js/paseo.js
@@ -1,33 +1,18 @@
-// let ciudad = document.getElementById("ciudad")
-// let godoyCruz = document.getElementById("godoyCruz")
-// let guaymallen = document.getElementById("guaymallen")
-// let maipu = document.getElementById("maipu")
-// let lujan = document.getElementById("lujan")
-// let lasHeras = document.getElementById("lasHeras")
-// let sanMartin = document.getElementById("sanMartin")
-// let sanRafael = document.getElementById("sanRafael")
-// let junin = document.getElementById("junin")
-// let rivadavia = document.getElementById("rivadavia")
-// let lavalle = document.getElementById("lavalle")
-// let laPaz = document.getElementById("laPaz")
-// let santaRosa = document.getElementById("santaRosa")
-// let tunuyan = document.getElementById("tunuyan")
-// let tupungato = document.getElementById("tupungato")
-// let generalAlvear = document.getElementById("generalAlvear")
-// let malargue = document.getElementById("malargue")
-// let sanCarlos = document.getElementById("ciudad")
-
-let arrPaseadores = [
+let arrPaseadoresEste = [
     {ciudad: "San Martin", nombre: "Juan", src: "./imagenes/juan.jpg", p1: "Edad: 31 años", p2: "3 años de experiencia"},
     {ciudad: "La Paz", nombre: "Sara", src: "./imagenes/sara.jpg", p1: "Edad: 28 años", p2: "5 años de experiencia"},
     {ciudad: "Rivadavia", nombre: "Denis", src: "./imagenes/denis.jpg", p1: "Edad: 30 años", p2: "2 años de experiencia"}
 ]
 
+let paseadoresCiudad = {nombre: "Fabián y Nicolás", src: "./imagenes/fabian-y-nicolas.jpg", p1: "Edad: 29 años", p2: "4 años de experiencia"};
+let paseadoresGodoyCruz = {nombre: "Sabrina", src: "./imagenes/sabrina.jpg", p1: "19 años", p2: "Primer año de experiencia"}
+let paseadoresMaipu = {nombre: "Ruben", src: "./imagenes/ruben.jpg", p1: "32 años", p2: "2 años de experiencia"}
+
 let paseadores = document.getElementById("paseadores")
 let cartaPaseadores = document.createElement("div")
 
 const buscador = () => {
-    for (const i of arrPaseadores) {
+    for (const i of arrPaseadoresEste) {
         switch(dptos.options[dptos.selectedIndex].value) {
             case "San Martin":
             case "La Paz":
@@ -48,11 +33,52 @@ const buscador = () => {
               `;
                 paseadores.append(cartaPaseadores)
                 break;
-            case "Lavalle":
-            case "Tupungato":
-            case "Tunuyán":
-            case "General Alvear":
-            case "Malargüe":
+            case "Ciudad de Mendoza":
+                cartaPaseadores.className = ("cartaPaseadores");
+                cartaPaseadores.innerHTML = `
+                <div class="card">
+                <img src="${paseadoresCiudad.src}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${paseadoresCiudad.nombre}</h5>
+                  <p class="card-text">${paseadoresCiudad.p1}</p>
+                  <p class="unidad"><small>${paseadoresCiudad.p2}</small></p>
+                  <button class="btn btn-primary">Contactar</button>
+                </div>
+              </div>
+              `;
+                paseadores.append(cartaPaseadores)
+                break;
+            case "Godoy Cruz":
+                cartaPaseadores.className = ("cartaPaseadores");
+                cartaPaseadores.innerHTML = `
+                <div class="card">
+                <img src="${paseadoresGodoyCruz.src}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${paseadoresGodoyCruz.nombre}</h5>
+                  <p class="card-text">${paseadoresGodoyCruz.p1}</p>
+                  <p class="unidad"><small>${paseadoresGodoyCruz.p2}</small></p>
+                  <button class="btn btn-primary">Contactar</button>
+                </div>
+              </div>
+              `;
+                paseadores.append(cartaPaseadores)
+                break;
+                case "Maipu":
+                    cartaPaseadores.className = ("cartaPaseadores");
+                    cartaPaseadores.innerHTML = `
+                    <div class="card">
+                    <img src="${paseadoresMaipu.src}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${paseadoresMaipu.nombre}</h5>
+                      <p class="card-text">${paseadoresMaipu.p1}</p>
+                      <p class="unidad"><small>${paseadoresMaipu.p2}</small></p>
+                      <button class="btn btn-primary">Contactar</button>
+                    </div>
+                  </div>
+                  `;
+                    paseadores.append(cartaPaseadores)
+                    break;
+            default:
                 cartaPaseadores.className = ("cartaSinPaseadores");
                 cartaPaseadores.innerHTML = `
                 <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
@@ -65,12 +91,6 @@ const buscador = () => {
                 `;
                 paseadores.append(cartaPaseadores)
                 break;
-            default:
-                cartaPaseadores.innerHTML = `
-                <h6>David</h6>
-                `
-                paseadores.append(cartaPaseadores)
-                break
         } 
     }
 }
